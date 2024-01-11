09bf0ad3e817f4799f318d18bcd07f3432aa3f00
Mahdi Noori <mahdinoori2389@gmail.com>
2024-01-11 18:50:22 +0430

    Add routes for the recipes and recipes details.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,17 @@ Rails.application.routes.draw do
   resources :users
   resources :foods
   get 'general_shopping_list', to: 'general_shopping_list#index'
+  get 'general_shopping_list' => 'general_shopping_list#index', as: 'shopping_list'
 
 
   resources :recipes
   get 'public_recipes' => 'recipes#public_recipes'
+
+  get 'public_recipes' => 'recipes#public_recipes'
+  get 'recipes/:id/add_ingredient' => 'recipe_foods#new', as:'add_recipe_food'
+  resources :recipes
+  # for updating the public 
+  patch 'recipes/:id/toggle_public', to: 'recipes#toggle_public', as: 'toggle_public_recipe'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
