e2e40cca6a9f3e8baaa942a1db33c01a213edeba
richie1988 <richiessikaonga@gmail.com>
2024-01-11 02:17:59 +0200

    Error handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   has_many :recipes, dependent: :destroy
   has_many :foods, dependent: :destroy
   has_many :recipe_foods, through: :recipes
